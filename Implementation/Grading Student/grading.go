450fdc7822aff94f13d0a973c68d2568fb6068bf
Ikhwan1358 <ikhwanushofa54@gmail.com>
2024-01-02 22:02:22 +0700

    implementation

---
+++ Implementation/Grading Student/grading.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as a parameter.
+ */
+
+func gradingStudents(grades []int32) []int32 {
+	var roundedGrades []int32
+
+	for _, grade := range grades {
+		roundedGrade := roundGrade(grade)
+		roundedGrades = append(roundedGrades, roundedGrade)
+	}
+
+	return roundedGrades
+}
+
+func roundGrade(grade int32) int32 {
+	if grade < 38 {
+		// No rounding needed if the grade is less than 38
+		return grade
+	}
+
+	// Calculate the next multiple of 5
+	nextMultipleOf5 := (grade/5 + 1) * 5
+
+	// Check if rounding is needed
+	if nextMultipleOf5-grade < 3 {
+		return nextMultipleOf5
+	}
+
+	return grade
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	gradesCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	var grades []int32
+
+	for i := 0; i < int(gradesCount); i++ {
+		gradesItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+		checkError(err)
+		gradesItem := int32(gradesItemTemp)
+		grades = append(grades, gradesItem)
+	}
+
+	result := gradingStudents(grades)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
